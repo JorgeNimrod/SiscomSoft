bc1787726058a0907700d5d3d52d11b7bc012408
Luis Dessens <Luis.fede23@gmail.com>
2017-08-14 18:49:30 -0700

    jiji prrr

--- SiscomSoft-Desktop/Views/FrmDetallePeriodo.Designer.cs
+++ SiscomSoft-Desktop/Views/FrmDetallePeriodo.Designer.cs
@@ -48,9 +48,10 @@
             // 
             this.cmbTurno.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.cmbTurno.FormattingEnabled = true;
+            this.cmbTurno.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbTurno.Items.AddRange(new object[] {
-            "Matutino",
-            "Vespertino"});
+            "MATUTINO",
+            "VESPERTINO"});
             this.cmbTurno.Location = new System.Drawing.Point(130, 54);
             this.cmbTurno.Name = "cmbTurno";
             this.cmbTurno.Size = new System.Drawing.Size(143, 26);
@@ -60,6 +61,7 @@
             // 
             this.txtFolio.Location = new System.Drawing.Point(12, 54);
             this.txtFolio.Name = "txtFolio";
+            this.txtFolio.ReadOnly = true;
             this.txtFolio.Size = new System.Drawing.Size(112, 26);
             this.txtFolio.TabIndex = 1;
             this.txtFolio.TextChanged += new System.EventHandler(this.txtFolio_TextChanged);
